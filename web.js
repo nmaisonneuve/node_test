b43ad53744b65cfba69820404259e8c40e9057c2
nicolas maisonneuve <n.maisonneuve@gmail.com>
2013-06-15 11:36:03 +0200

    test hook

--- web.js
+++ web.js
@@ -1,5 +1,11 @@
 var express = require('express');
+//var hookshot = require('hookshot');
 var pg = require('pg');
+var spawn = require('child_process').spawn;
+
+function shspawn(command) {
+   spawn('sh', ['-c', command], { stdio: 'inherit' });
+}
 
 cnxString = process.env.DATABASE_URL ||  "tcp://nico:astiko@localhost/gsv_cutter";
 client = new pg.Client(cnxString);
@@ -10,10 +16,17 @@ var app = express();
 app.get('/', function (request, response) {
   client.query('SELECT NOW() AS "theTime"', function(err, result) {
       //console.log(result.rows[0].theTime);
-      response.send("Hello World, time from the postgresql db:" + result.rows[0].theTime );
+      response.send("Hello World , time from the postgresql db:" + result.rows[0].theTime );
       //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
   })
 });
+
+app.get('/my-github-hook',function(req, res) {
+  shspawn("git pull && npm install");
+  res.send("");
+});
+
+
 
 
 app.get('/coucou/:name', function(req, res) {
