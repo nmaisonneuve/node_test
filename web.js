e0c6ad41a0d54d36fbabf034332e8e3315196a2d
nicolas maisonneuve <n.maisonneuve@gmail.com>
2013-06-15 12:09:38 +0200

    change port

--- web.js
+++ web.js
@@ -34,8 +34,7 @@ app.get('/coucou/:name', function(req, res) {
     res.send(result);
 });
 
-var port = process.env.PORT || 5000;
-
+var port = 3300; // process.env.PORT || 5000;
 app.listen(port, function () {
     console.log('Listening on ' + port);
 });
